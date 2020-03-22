098f6a0e11aa0e8d77c786bd011834b63fa01454
Mohammad Abdullatif <m.abdullatif@qbs.jo>
2020-03-22 11:36:34 +0200

    add test cases to implement matrix class

---
+++ matrix-utility/src/main/java/jo/edu/htu/utils/Matrix.java
@@ -0,0 +1,27 @@
+package jo.edu.htu.utils;
+
+public class Matrix {
+    public Matrix(int[][] matrix) {
+    }
+
+    public int rows() {
+        return -1;
+    }
+
+    public int cols() {
+        return -1;
+    }
+
+    public int value(int row, int col) {
+        return 0;
+    }
+
+    public void forEach(ValueConsumer valueConsumer) {
+
+    }
+
+    public interface ValueConsumer {
+
+        void accept(int row, int col, int value);
+    }
+}
---
+++ matrix-utility/src/test/java/jo/edu/htu/utils/MatrixTest.java
@@ -0,0 +1,81 @@
+package jo.edu.htu.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class MatrixTest {
+
+    @Test
+    public void givenArrayWithNullRow_whenContructMatrix_thenFail() {
+        int[][] matrix = new int[][]{
+                {3, 4, 5},
+                {9, 6, 4},
+                {10, 6, 7}
+        };
+        int rowToCorrupt = new Random().nextInt(matrix.length);
+        matrix[rowToCorrupt] = null;
+        IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Matrix(matrix), "The constructor should throw IllegalArgumentException");
+        Assertions.assertEquals("row " + rowToCorrupt + " is null", thrown.getMessage(),
+                "the exception message is not as expected");
+    }
+
+    @Test
+    public void givenArrayWithInConsistentRows_whenContructMatrix_thenFail() {
+        int[][] matrix = new int[][]{
+                {3, 4, 5},
+                {9, 6, 4},
+                {10, 6, 7}
+        };
+        int rowToCorrupt = new Random().nextInt(matrix.length);
+        matrix[rowToCorrupt] = Arrays.copyOf(matrix[rowToCorrupt], rowToCorrupt + 1);
+        IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Matrix(matrix), "The constructor should throw IllegalArgumentException");
+        Assertions.assertEquals("Inconsistent rows", thrown.getMessage(),
+                "the exception message is not as expected");
+    }
+
+
+    @Test
+    public void givenAValidArrayWhenConstructing_thenMatrixInstanceIsCreated() {
+        int[][] array = {
+                {3, 4, 6},
+                {9, 8, 7},
+                {90, 100, 90},
+                {6, 8, 9}
+        };
+        Matrix matrix = new Matrix(array);
+
+        Assertions.assertEquals(array.length, matrix.rows(), "invalid rows count");
+        Assertions.assertEquals(array[0].length, matrix.cols(), "invalid rows count");
+        Random random = new Random();
+        int invalidRow = array.length + random.nextInt(3);
+        int invalidCol = array[0].length + random.nextInt(array[0].length);
+        IllegalArgumentException
+                thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> matrix.value(invalidRow, invalidCol));
+        Assertions.assertEquals("invalid cell index: " + invalidRow + "," + invalidCol, thrown.getMessage());
+
+        for (int row = 0; row < array.length; row++) {
+            for (int col = 0; col < array[row].length; col++) {
+                Assertions.assertEquals(array[row][col], matrix.value(row, col));
+            }
+        }
+
+        int row = random.nextInt(array.length);
+        int col = random.nextInt(array[0].length);
+        int oldValue = array[row][col];
+        array[row][col] = oldValue - 10;
+        Assertions.assertTrue(matrix.value(row, col) != array[row][col], "I was able to modify your matrix from outside");
+        array[row][col] = oldValue;
+
+        int[][] consumed = new int[array.length][array[0].length];
+        matrix.forEach((r, c, v) -> {
+            consumed[r][c] = v;
+        });
+        Assertions.assertArrayEquals(array, consumed, "it seems not all cells were visited");
+
+    }
+}
