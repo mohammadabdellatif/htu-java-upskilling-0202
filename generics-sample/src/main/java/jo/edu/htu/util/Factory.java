2fe319148b246d7e288a04429ba96dd3e7a60be5
Mohammad Abdullatif <mohammad.s.abdellatif@gmail.com>
2020-03-28 10:41:43 +0300

    add another sample for interfaces

--- generics-sample/src/main/java/jo/edu/htu/util/Factory.java
+++ generics-sample/src/main/java/jo/edu/htu/util/Factory.java
@@ -1,6 +1,6 @@
 package jo.edu.htu.util;
 
-public interface Factory<ITEM extends Stationery> {
+public interface Factory<ITEM> {
 
     ITEM newItem();
 }
@@ -18,5 +18,19 @@ class PencilFactory implements Factory<Pencil> {
     @Override
     public Pencil newItem() {
         return new Pencil();
+    }
+}
+
+class SingleItemFactory<T> implements Factory<T> {
+
+    private T item;
+
+    SingleItemFactory(T item) {
+        this.item = item;
+    }
+
+    @Override
+    public T newItem() {
+        return item;
     }
 }
