765ac69af5c5ed80f786c402438fedf88d96ba78
maisalsmadi <mayssmadi230@gmail.com>
2020-03-29 14:22:39 -0700

    try to solve matrix utility

--- matrix-utility/src/main/java/jo/edu/htu/utils/Matrix.java
+++ matrix-utility/src/main/java/jo/edu/htu/utils/Matrix.java
@@ -1,29 +1,44 @@
 package jo.edu.htu.utils;
 
+import java.util.ArrayList;
+
 public class Matrix {
+    int[][] matrix;
+    int row;
+    int col;
+
     public Matrix(int[][] matrix) {
         for (int row = 0; row < matrix.length; row++) {
+
             if (matrix[row] == null)
                 throw new IllegalArgumentException("row " + row + " is null");
             if (matrix[row].length != matrix[0].length)
                 throw new IllegalArgumentException("Inconsistent rows");
         }
+
+
     }
 
     public int rows() {
-        return -1;
+        return row;
     }
 
     public int cols() {
-        return -1;
+        return col;
     }
 
     public int value(int row, int col) {
-        return 0;
+        if (row>matrix.length)
+            throw new IllegalArgumentException("invalid cell index: " + row + "," + col);
+            return matrix[row][col];
     }
 
-    public void forEach(ValueConsumer valueConsumer) {
 
+    public void forEach(ValueConsumer valueConsumer) {
+     for (row=0;row<matrix.length;row++)
+     for (int col = 0; col <matrix[row].length ; col++) {
+        valueConsumer.accept(row,col,value(row,col));
+    }
     }
 
     public interface ValueConsumer {
