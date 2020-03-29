a9bf0cf03323d6c14c9a857053b6009a2e5fc92e
AnasIbeed <61617125+AnasIbeed@users.noreply.github.com>
2020-03-30 01:12:27 +0300

    Update Stack.java

--- generics-practice/src/main/java/jo/edu/htu/upskilling/utils/Stack.java
+++ generics-practice/src/main/java/jo/edu/htu/upskilling/utils/Stack.java
@@ -1,23 +1,40 @@
 package jo.edu.htu.upskilling.utils;
 
+import java.util.ArrayList;
+
 public class Stack<ELEMENT> {
+    private final int capacity;
+    ArrayList<ELEMENT> stack = new ArrayList<>();
 
     public Stack(int capacity) {
+        if (capacity < 0) {
+            throw new IllegalArgumentException("It must be positive");
+        }
+        stack.ensureCapacity(capacity);
+        this.capacity = capacity;
     }
 
     public int capacity() {
-        return -1;
+        return capacity;
     }
 
     public int count() {
-        return -1;
+        return stack.size();
     }
 
     public ELEMENT pop() {
-        return null;
+        if (count() == 0) {
+            throw new IllegalArgumentException("Stack is empty");
+        }
+        ELEMENT pop = stack.remove(stack.size() - 1);
+        return pop;
     }
 
     public void push(ELEMENT element) {
+        if (capacity()>=stack.size()){
+            throw new IllegalArgumentException("Stack is full");
+        }
+        stack.add(element);
 
     }
 }
