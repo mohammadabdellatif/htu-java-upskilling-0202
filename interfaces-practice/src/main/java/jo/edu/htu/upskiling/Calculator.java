b3fe7d3c38e4f70a50fb37f12280133fd76003e0
bayanalzboon <bnihassan009@gmail.com>
2020-03-30 16:27:20 +0300

    interface practice
    task 3

--- interfaces-practice/src/main/java/jo/edu/htu/upskiling/Calculator.java
+++ interfaces-practice/src/main/java/jo/edu/htu/upskiling/Calculator.java
@@ -8,18 +8,28 @@ public class Calculator<OPERAND> {
     }
 
     public OPERAND sum(OPERAND first, OPERAND second) {
-        return null;
+
+        Operation<OPERAND> summation = operationsFactory.summation( );
+
+        return summation.execute(first,second);
     }
 
     public OPERAND subtract(OPERAND operand1, OPERAND operand2) {
-        return null;
+        Operation<OPERAND> subtract = operationsFactory.subtraction();
+
+        return subtract.execute(operand1,operand2);
     }
 
     public OPERAND multiply(OPERAND operand1, OPERAND operand2) {
-        return null;
+        Operation<OPERAND> multiply = operationsFactory.multiplication();
+
+        return multiply.execute(operand1,operand2);
+
     }
 
     public OPERAND divide(OPERAND first, OPERAND second) {
-        return null;
+        Operation<OPERAND> divide = operationsFactory.division();
+
+        return divide.execute(first,second);
     }
 }
--- interfaces-practice/src/main/java/jo/edu/htu/upskiling/IntegerOperationsFactory.java
+++ interfaces-practice/src/main/java/jo/edu/htu/upskiling/IntegerOperationsFactory.java
@@ -1,24 +1,41 @@
+
 package jo.edu.htu.upskiling;
 
 public class IntegerOperationsFactory implements OperationsFactory<Integer> {
     // TODO you should implement this class
+    int first, second ;
+
+
+
     @Override
-    public Operation<Integer> summation() {
-        return null;
+    public Operation<Integer> summation(){
+        Operation operation=  new Operation( ) {
+
+
+
+            @Override
+            public Integer execute( Object first, Object second ) {
+                return Integer.parseInt(String.valueOf(first))+Integer.parseInt(String.valueOf(second));
+            }};
+
+        return operation;
     }
+
 
     @Override
     public Operation<Integer> subtraction() {
-        return null;
+       Operation subtractionOp = (first, second) -> Integer.parseInt(String.valueOf(first))-Integer.parseInt(String.valueOf(second));
+    return subtractionOp;
     }
-
     @Override
     public Operation<Integer> multiplication() {
-        return null;
+        Operation multiplicationOp = (first, second) -> Integer.parseInt(String.valueOf(first))*Integer.parseInt(String.valueOf(second));
+        return multiplicationOp;
     }
 
     @Override
     public Operation<Integer> division() {
-        return null;
+        Operation divisionOp = (first, second) -> Integer.parseInt(String.valueOf(first))/Integer.parseInt(String.valueOf(second));
+        return divisionOp;
     }
 }
