28c86ed135dca401e02fbca3b4808f130d248430
maisalsmadi <mayssmadi230@gmail.com>
2020-04-01 00:20:23 -0700

    Udate to the first two unit in Stack(succes)

--- generics-practice/src/main/java/jo/edu/htu/upskilling/utils/Stack.java
+++ generics-practice/src/main/java/jo/edu/htu/upskilling/utils/Stack.java
@@ -1,30 +1,57 @@
 package jo.edu.htu.upskilling.utils;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Stack<ELEMENT> {
-    int capacity;
+    Object[] array;
+    int pointer;
+    private int size;
+
     public Stack(int capacity) {
-        this.capacity=capacity;
+        if (capacity < 0)
+            throw new IllegalArgumentException("negative size");
+        array = new Object[capacity];
+        pointer=size;
     }
 
-    public int capacity()  {
-        if (capacity<0)
-    throw new IllegalArgumentException("negative size");
-        if (capacity==0)
-            pop();
-        return 0;
+    public int capacity() {
+        return array.length;
     }
 
     public int count() {
-        return -1;
+        return size;
+
     }
 
     public ELEMENT pop() {
-        if (capacity==0)
+        if (array.length == 0)
             throw new IllegalStateException("stack is empty");
-        return null;
+        if (pointer == 0)
+            throw new IllegalStateException("stack is empty");
+        pointer--;
+        return (ELEMENT) array[pointer];
     }
 
     public void push(ELEMENT element) {
+        if (pointer == size)
+            throw new IllegalStateException("stack is full");
+        size = 1;
 
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Stack<?> stack = (Stack<?>) o;
+        return size == stack.size &&
+                Arrays.equals(array, stack.array);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(array);
+    }
 }
+
