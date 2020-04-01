eb3a1b332315d03498d1aa54793074b73d2a3122
Wafaa20 <61623494+Wafaa20@users.noreply.github.com>
2020-04-01 08:51:54 +0300

    update on stack

--- generics-practice/src/main/java/jo/edu/htu/upskilling/utils/Stack.java
+++ generics-practice/src/main/java/jo/edu/htu/upskilling/utils/Stack.java
@@ -1,26 +1,55 @@
 package jo.edu.htu.upskilling.utils;
-
 import java.util.Iterator;
+import java.util.Objects;
 
 public class Stack<ELEMENT> implements Iterable<ELEMENT>  {
+    private Object stackarry[];
+    private int size;
+    ELEMENT[] elements;
+    int stackCapacity;
 
     public Stack(int capacity) {
+        size = 0;
+        if (capacity < 0) {
+            throw new IllegalArgumentException("negative size");
+        }
+        stackarry = (ELEMENT[]) (new Object[capacity]);
+        stackCapacity = capacity;
     }
 
     public int capacity() {
-        return -1;
+        return stackCapacity;
     }
 
     public int count() {
-        return -1;
+        return size;
     }
 
     public ELEMENT pop() {
-        return null;
+        if (size == 0) {
+            throw new IllegalStateException("stack is empty");
+        }
+        return elements[size--];
     }
 
     public void push(ELEMENT element) {
-
+        if (stackCapacity == size) {
+            throw new IllegalStateException("satck is full");
+        }
+        stackarry[size] = element;
+        size = -1;
+    }
+    @Override
+    public boolean equals(Object o) {
+        Stack stack = (Stack) o;
+        if (stack == null) {
+            return false;
+        }
+        return true;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(stackarry);
     }
 
     @Override
