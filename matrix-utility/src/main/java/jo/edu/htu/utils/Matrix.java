b5d8983fba95a89eda2862c509d7761581db4bb4
AnasIbeed <61617125+AnasIbeed@users.noreply.github.com>
2020-04-17 19:12:25 +0300

    Update Matrix.java

--- matrix-utility/src/main/java/jo/edu/htu/utils/Matrix.java
+++ matrix-utility/src/main/java/jo/edu/htu/utils/Matrix.java
@@ -1,33 +1,45 @@
 package jo.edu.htu.utils;
 
 public class Matrix {
+    int [][] matrix ;
     public Matrix(int[][] matrix) {
         for (int row = 0; row < matrix.length; row++) {
             if (matrix[row] == null)
-                throw new IllegalArgumentException("row " + row + " is null");
             if (matrix[row].length != matrix[0].length)
-                throw new IllegalArgumentException("Inconsistent rows");
+                        throw new IllegalArgumentException("Inconsistent rows");
+            }
+            this.matrix=new int[matrix.length][matrix[0].length];
         }
-    }
 
-    public int rows() {
-        return -1;
-    }
+        public int rows() {
 
-    public int cols() {
-        return -1;
-    }
+            return matrix.length;
+        }
 
-    public int value(int row, int col) {
-        return 0;
-    }
+        public int cols() {
 
-    public void forEach(ValueConsumer valueConsumer) {
+            return matrix[0].length;
+        }
 
-    }
+        public int value(int row, int col) {
 
-    public interface ValueConsumer {
+            if (row <0 || col <0)
+                throw new IllegalArgumentException("it must be positive ");
+            if ((row >= matrix.length) || ( col >= matrix[0].length))
+                throw new IllegalArgumentException("cell index not exist: (" + row + ',' + col+')');
 
-        void accept(int row, int col, int value);
-    }
-}
+            return matrix[row][col];
+        }
+
+        public void forEach(ValueConsumer valueConsumer) {
+
+            for (int row = 0; row < matrix.length; row++) {
+                for (int col = 0; col < matrix[row].length; col++) {
+                    valueConsumer.accept(row, col, matrix[row][col]);
+                }
+            }
+        }
+
+        public interface ValueConsumer {
+            void accept(int row, int col, int value);
+        }}
