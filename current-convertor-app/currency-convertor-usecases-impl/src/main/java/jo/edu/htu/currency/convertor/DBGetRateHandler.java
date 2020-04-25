9b8bd472282ad9ead170c4ef5c509ce26dc7119b
Mohammad Abdullatif <mohammad.s.abdellatif@gmail.com>
2020-04-25 11:41:27 +0300

    add get rate handler from db

---
+++ current-convertor-app/currency-convertor-usecases-impl/src/main/java/jo/edu/htu/currency/convertor/DBGetRateHandler.java
@@ -0,0 +1,9 @@
+package jo.edu.htu.currency.convertor;
+
+public class DBGetRateHandler implements GetRateHandler {
+
+    @Override
+    public GetRateResult getRate(GetRateRequest request) {
+        return null;
+    }
+}
