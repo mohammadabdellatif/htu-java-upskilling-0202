885f38723d3b7170fe718b5dcc29eb21e20560c2
Mohammad Abdullatif <mohammad.s.abdellatif@gmail.com>
2020-04-28 18:16:25 +0300

    transaction sample

--- upskilling-projects/jdbc-samples/src/main/java/jo/edu/htu/jdbc/SQLConsole.java
+++ upskilling-projects/jdbc-samples/src/main/java/jo/edu/htu/jdbc/SQLConsole.java
@@ -3,7 +3,7 @@ package jo.edu.htu.jdbc;
 import java.sql.*;
 import java.util.Scanner;
 
-public class QueryDatabase2 {
+public class SQLConsole {
 
     public static void main(String[] args) throws SQLException {
         String url = "jdbc:mysql://localhost:3306/countries?serverTimezone=UTC";
@@ -11,11 +11,23 @@ public class QueryDatabase2 {
         String password = "root";
         Scanner scanner = new Scanner(System.in);
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            connection.setAutoCommit(false);
+            connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
             try (Statement statement = connection.createStatement()) {
                 while (true) {
                     System.out.print("mysql>");
                     String sql = scanner.nextLine();
-                    if("exit".equalsIgnoreCase(sql)){
+                    if (sql.endsWith(";"))
+                        sql = sql.substring(0, sql.length() - 1);
+                    if (sql.equalsIgnoreCase("commit")) {
+                        connection.commit();// commit all changes to the database
+                        continue;
+                    }
+                    if (sql.equalsIgnoreCase("rollback")) {
+                        connection.rollback();
+                        continue;
+                    }
+                    if ("exit".equalsIgnoreCase(sql)) {
                         break;
                     }
                     boolean query = statement.execute(sql);// general execute method
