66bee940dfedf2759a6fb2b9a3d9ecba890b078f
Mohammad Abdullatif <mohammad.s.abdellatif@gmail.com>
2020-05-02 13:14:12 +0300

    complete the servlet example

--- current-convertor-app/currency-convertor-webapp/src/main/java/jo/edu/htu/convertor/web/ListRatesServlet.java
+++ current-convertor-app/currency-convertor-webapp/src/main/java/jo/edu/htu/convertor/web/ListRatesServlet.java
@@ -5,28 +5,33 @@ import jo.edu.htu.currency.model.DBExchangeRateRepository;
 import jo.edu.htu.currency.model.ExchangeRateRepository;
 import org.apache.commons.dbcp2.BasicDataSource;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.math.BigDecimal;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Map;
 
-@WebServlet(name = "ListRatesServlet", urlPatterns = "/rates")
+//@WebServlet(name = "ListRatesServlet", urlPatterns = "/rates", loadOnStartup = 1)
 public class ListRatesServlet extends HttpServlet {
     private ListRatesHandler ratesHandler;
+    private BasicDataSource dataSource;
 
-    public ListRatesServlet() {
+    @Override
+    public void init() throws ServletException {
+        System.out.println("servlet is initialized");
         this.ratesHandler = prepareListRatesHandler();
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("handle service request");
         HashSet<String> toCodes = new HashSet<>();
         String fromCode = request.getParameter("fromCode");
         String[] toCodesParams = request.getParameterValues("toCodes");
@@ -50,13 +55,24 @@ public class ListRatesServlet extends HttpServlet {
     }
 
     private ListRatesHandler prepareListRatesHandler() {
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setUrl("jdbc:mysql://localhost:3306/countries?serverTimezone=UTC");
-        dataSource.setUsername("root");
-        dataSource.setPassword("root");
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource = new BasicDataSource();
+        ServletConfig servletConfig = getServletConfig();
+        dataSource.setUrl(servletConfig.getInitParameter("db_url"));
+        dataSource.setUsername(servletConfig.getInitParameter("db_user"));
+        dataSource.setPassword(servletConfig.getInitParameter("db_pass"));
+        dataSource.setDriverClassName(servletConfig.getInitParameter("db_driver"));
         ExchangeRateRepository repository = new DBExchangeRateRepository(dataSource);
         GetRateHandler getRateHandler = new DBGetRateHandler(repository);
         return new DefaultListRatesHandler(getRateHandler);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("servlet to be destroyed");
+        try {
+            dataSource.close();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
