0bcd58478b821113c01e003f39d7ca01ddc492b1
Mohammad Abdullatif <mohammad.s.abdellatif@gmail.com>
2020-05-04 21:59:03 +0300

    add missing fields

--- noaa-parent/noaa-model/src/main/java/jo/edu/htu/noaa/GlobalSummaryOfDay.java
+++ noaa-parent/noaa-model/src/main/java/jo/edu/htu/noaa/GlobalSummaryOfDay.java
@@ -1,4 +1,70 @@
 package jo.edu.htu.noaa;
 
+import java.time.LocalDate;
+
 public class GlobalSummaryOfDay {
+
+    private String wban;
+    private String usaf;
+    private LocalDate date;
+    private double temperature;
+    private double maxTemp;
+    private double minTemp;
+    private double windSpeed;
+
+    public String getWban() {
+        return wban;
+    }
+
+    public void setWban(String wban) {
+        this.wban = wban;
+    }
+
+    public String getUsaf() {
+        return usaf;
+    }
+
+    public void setUsaf(String usaf) {
+        this.usaf = usaf;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getMaxTemp() {
+        return maxTemp;
+    }
+
+    public void setMaxTemp(double maxTemp) {
+        this.maxTemp = maxTemp;
+    }
+
+    public double getMinTemp() {
+        return minTemp;
+    }
+
+    public void setMinTemp(double minTemp) {
+        this.minTemp = minTemp;
+    }
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
 }
--- noaa-parent/noaa-model/src/main/java/jo/edu/htu/noaa/Station.java
+++ noaa-parent/noaa-model/src/main/java/jo/edu/htu/noaa/Station.java
@@ -1,4 +1,88 @@
 package jo.edu.htu.noaa;
 
+import java.time.LocalDate;
+
 public class Station {
+
+    private String usaf;
+    private String wban;
+    private String name;
+    private String country;
+    private String state;
+    private double latitude;
+    private double longitude;
+    private LocalDate begin;
+    private LocalDate end;
+
+    public String getUsaf() {
+        return usaf;
+    }
+
+    public void setUsaf(String usaf) {
+        this.usaf = usaf;
+    }
+
+    public String getWban() {
+        return wban;
+    }
+
+    public void setWban(String wban) {
+        this.wban = wban;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public LocalDate getBegin() {
+        return begin;
+    }
+
+    public void setBegin(LocalDate begin) {
+        this.begin = begin;
+    }
+
+    public LocalDate getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalDate end) {
+        this.end = end;
+    }
 }
